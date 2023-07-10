8a86d043080494c848a91b9988576a5e1f092d05
OUSSAMA OULOUARK <alioulouark@gmail.com>
2023-07-10 11:22:15 +0100

    Tsak 02

--- lib/Activites/Screen.dart
+++ lib/Activites/Screen.dart
@@ -1,36 +1,69 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 
 class Screen extends StatelessWidget {
   Screen();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Hellow Guys"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Container(
-          width: double.infinity,
-          color: const Color.fromARGB(200, 176, 133, 141),
-          child: const Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Column(
-              children: [
-                Row(
+    return FutureBuilder(
+      future: _loadTraineesData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error loading data.'),
+            );
+          }
 
-                )
+          List<dynamic> traineesData = snapshot.data!;
+          int totalNames = traineesData.length;
 
-              ],
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("World Skills"),
             ),
+            body: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                width: double.infinity,
+                color: const Color.fromARGB(200, 176, 133, 141),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Center(
+                              child: Text(
+                                "Total number of names: $totalNames",
+                                style: TextStyle(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
 
-          ),
-        ),
-      ),
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
     );
   }
 
+  Future<List<dynamic>> _loadTraineesData() async {
+    String traineesAsJsonString = await rootBundle.loadString("assets/trainees.json");
+    List<dynamic> traineesData = jsonDecode(traineesAsJsonString);
 
+    return traineesData;
+  }
 }
